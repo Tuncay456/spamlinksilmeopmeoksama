79d88697d168438ec7a23839d8c199d8e1065e0f
Tuncay456 <106577872+Tuncay456@users.noreply.github.com>
2022-06-05 23:23:51 +0300

    Update service.py

--- service.py
+++ service.py
@@ -4,9 +4,9 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, User, Mes
 
 Client = Client(
     "Service message remover",
-    bot_token = os.environ["BOT_TOKEN"],
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"]
+    bot_token = os.environ["5313905202:AAGMNPbofxcj0RjcjdSP1zrgu8gUd5mAqWE"],
+    api_id = int(os.environ["15781409"]),
+    api_hash = os.environ["0499f7f54f3fbb56b43acb6edf2d9696"]
 )
 
 START_BUTTON = InlineKeyboardMarkup(
